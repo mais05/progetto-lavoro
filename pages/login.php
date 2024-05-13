a6c0c33999b7da3c6d0bfba546551bc3c4f50049
Simone Giolai <simonegiolai74@gmail.com>
2024-05-13 12:33:56 +0200

    ✨login

--- pages/login.php
+++ pages/login.php
@@ -2,7 +2,7 @@
     <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 class="text-2xl font-semibold mb-4">Login</h2>
 
-        <form action="#" method="POST">
+        <form action="config/login.php" method="POST">
             <div class="mb-4">
                 <label for="email" class="block text-gray-700">Email/Username</label>
                 <input type="email" id="email" name="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
