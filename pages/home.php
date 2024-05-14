c7542276491177f132ce85fb9c03828b19ec3591
Simone Giolai <simonegiolai74@gmail.com>
2024-05-14 10:06:37 +0200

    💄 update UI

--- pages/home.php
+++ pages/home.php
@@ -1,30 +1,11 @@
-<div class="min-w-full mx-auto fixed bottom-0 z-40">
-    <div class="container max-w-3xl bg-gray-800 mx-auto pb-2">
-        <div class="flex justify-around">
-            <a href="/" class="flex flex-col items-center justify-center py-3 px-3 text-white">
-                <div class="rounded-full bg-gray-900 p-3">
-                    <span class="text-2xl">🏠</span>
-                </div>
-                <span class="text-xs font-bold mt-1">Home</span>
-            </a>
-            <a href="/news/" class="flex flex-col items-center justify-center py-3 px-3 text-white">
-                <div class="rounded-full bg-gray-900 p-3">
-                    <span class="text-2xl">📰</span>
-                </div>
-                <span class="text-xs font-bold mt-1">News</span>
-            </a>
-            <a href="/feedbacks/" class="flex flex-col items-center justify-center py-3 px-3 text-white">
-                <div class="rounded-full bg-gray-900 p-3">
-                    <span class="text-2xl">📣</span>
-                </div>
-                <span class="text-xs font-bold mt-1">Feedback</span>
-            </a>
-            <a href="/services/" class="flex flex-col items-center justify-center py-3 px-3 text-white">
-                <div class="rounded-full bg-gray-900 p-3">
-                    <span class="text-2xl">🛠️</span>
-                </div>
-                <span class="text-xs font-bold mt-1">Services</span>
-            </a>
-        </div>
-    </div>
-</div>
+<?php 
+
+define("NAVIGATION_PAGE", "home"); 
+
+//TODO require_once "includes/components/templates/template.php";
+
+?>
+
+
+<?php //TODO template_HTML("calendars/caledars.php");?> 
+<?php include "includes/components/structure/navigations/main/bottom.php";?>
